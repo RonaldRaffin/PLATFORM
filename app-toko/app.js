0061374a85e3de8e5e8ce86086f5cf655cca2083
ronal <sabikaliyee@gmail.com>
2026-04-15 17:42:57 +0700

    first commit

---
+++ app-toko/app.js
@@ -0,0 +1,26 @@
+async function ambilDataBarang() {
+    try {
+        const response = await fetch('http://localhost:8080/platform/api-toko/get_barang.php');
+        const hasil = await response.json();
+
+        if (hasil.status === 'success') {
+            let html = '';
+
+            hasil.data.forEach(barang => {
+                html += `
+                    <tr class="text-center border-b">
+                        <td class="p-2">${barang.id}</td>
+                        <td class="p-2">${barang.nama_barang}</td>
+                        <td class="p-2">Rp ${barang.harga}</td>
+                    </tr>
+                `;
+            });
+
+            document.getElementById('tabel-barang').innerHTML = html;
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+ambilDataBarang();
